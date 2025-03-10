e38731820a5c6bbee6cca24b6edc46f4d61a4b00
nyel-101 <dandevjr@gmail.com>
2025-03-10 00:19:54 +0000

    refactor(config): enhance auth tokens

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f8fbec9b88246189d0b6d8d25e45403 = () => {
-  return '2026-02-25T22:47:32Z';
+const node_341ee82956a14ceabec649c9711905bb = () => {
+  return '2025-03-10T00:19:54Z';
 };
