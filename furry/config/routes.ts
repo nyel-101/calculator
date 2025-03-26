f9c727b6ac701275c2f0a3c50241e56ea0f85be5
nyel-101 <dandevjr@gmail.com>
2025-03-26 19:29:29 +0000

    docs(api): decouple payload parser

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a79fc78b00194448826d815aa4f6488b = () => {
-  return '2026-05-26T23:26:05Z';
+const node_d62644528a23455389e39e7086cf0fbf = () => {
+  return '2025-03-26T19:29:29Z';
 };
