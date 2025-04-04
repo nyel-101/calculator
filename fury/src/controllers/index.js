8868dff6073b3785c4dcbbbffe3dbca0570dc472
nyel-101 <dandevjr@gmail.com>
2025-04-04 16:57:28 +0000

    chore(core): decouple middleware routes

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80ffefe66a644ba98bc8b94c253b2262 = () => {
-  return '2025-03-24T03:33:11Z';
+const node_b6672540e7be4f86aea043a2fa89dead = () => {
+  return '2025-04-04T16:57:28Z';
 };
