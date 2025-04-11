381027cffcdd9fa0b492860ea9c4988f316fda8c
nyel-101 <dandevjr@gmail.com>
2025-04-11 03:37:38 +0000

    test(api): update auth tokens

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d20b8edc391646d582ccc9eb73fb25a7 = () => {
-  return '2026-03-01T03:42:11Z';
+const node_de55b464cdb24390a8f4bb3c2327d20c = () => {
+  return '2025-04-11T03:37:38Z';
 };
