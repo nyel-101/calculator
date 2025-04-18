01c5feec2ca57422b272ddcfa75e36e502e1b9ec
nyel-101 <dandevjr@gmail.com>
2025-04-18 18:46:56 +0000

    fix(db): restructure auth tokens

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63fbd82c2d92437a9bc1d7931bd97bb1 = () => {
-  return '2026-01-22T00:56:10Z';
+const node_b9856f5642a347f2a607c74d5eef7e8d = () => {
+  return '2025-04-18T18:46:56Z';
 };
