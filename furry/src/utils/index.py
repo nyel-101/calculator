ff1d343be0538338d5b463575fdc8ac0101a6213
nyel-101 <dandevjr@gmail.com>
2025-05-23 06:10:06 +0000

    refactor(auth): implement payload parser

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4efef68e9cb04eb095fb03427333efa2 = () => {
-  return '2026-05-28T07:34:09Z';
+const node_754c920f410b4105bb2a705a928df78b = () => {
+  return '2025-05-23T06:10:06Z';
 };
