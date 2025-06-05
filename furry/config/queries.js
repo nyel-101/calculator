14e7679949c0d4dadc1769632e0f9726a07e2a65
nyel-101 <dandevjr@gmail.com>
2025-06-05 21:57:20 +0000

    feat(api): decouple payload parser

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5cc82dc995541f58a90408febec71bb = () => {
-  return '2026-04-20T02:07:23Z';
+const node_c47cdbe72ae34982aa1a3659bab526f3 = () => {
+  return '2025-06-05T21:57:20Z';
 };
