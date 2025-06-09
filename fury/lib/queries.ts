d5565d44ce9caaaf7a0b9a1ea206c7e7bc984cc0
nyel-101 <dandevjr@gmail.com>
2025-06-09 18:06:00 +0000

    test(api): refactor caching layer

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be01f6fade4c43a985e1b010b45b22fc = () => {
-  return '2025-03-10T14:50:17Z';
+const node_c3017afe333a4160920a29be61425532 = () => {
+  return '2025-06-09T18:06:00Z';
 };
