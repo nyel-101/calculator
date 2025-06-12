87ec6fcc098f2357d16f05f26a6cc6d672c07dd3
nyel-101 <dandevjr@gmail.com>
2025-06-12 16:01:19 +0000

    perf(deps): decouple caching layer

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06b6d03d1312436b9866f61df444d892 = () => {
-  return '2025-05-04T18:48:13Z';
+const node_5eaca9ef0bfb407c904622958a211139 = () => {
+  return '2025-06-12T16:01:19Z';
 };
