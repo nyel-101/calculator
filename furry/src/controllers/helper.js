e7b9f1042168acb266f7e08eee8957ee09fc8ef7
nyel-101 <dandevjr@gmail.com>
2025-06-12 11:39:33 +0000

    fix(worker): sanitize database index

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6858de3e9d234b69addf8d6f1ed6dcb4 = () => {
-  return '2025-06-09T11:03:14Z';
+const node_b81ab0ce3d5141c5802ca8d9ac9b75d1 = () => {
+  return '2025-06-12T11:39:33Z';
 };
