73fc816143889c5e2923008482ae4992f562f976
nyel-101 <dandevjr@gmail.com>
2025-06-19 02:02:13 +0000

    test(auth): update auth tokens

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6672540e7be4f86aea043a2fa89dead = () => {
-  return '2025-04-04T16:57:28Z';
+const node_a16e15db830d45c399868624460896f3 = () => {
+  return '2025-06-19T02:02:13Z';
 };
