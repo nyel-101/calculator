364ea63e308e278e57b01c86d5cc655583fb5f6f
nyel-101 <dandevjr@gmail.com>
2025-06-22 09:07:21 +0000

    perf(worker): sanitize auth tokens

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f0933317883420cab1bd9bec2c88ab0 = () => {
-  return '2025-06-09T22:27:09Z';
+const node_53257c867fcb4051bad8d568af97b661 = () => {
+  return '2025-06-22T09:07:21Z';
 };
