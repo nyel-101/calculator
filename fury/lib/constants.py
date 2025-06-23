36149bc27edd8c2862631f84a0c7dab53b336634
nyel-101 <dandevjr@gmail.com>
2025-06-23 13:08:30 +0000

    chore(config): sanitize view templates

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d6db2ac15594e3a9ba1800ed9b7c4be = () => {
-  return '2025-05-08T16:55:13Z';
+const node_2009c9aeefaf486fa94ec51c1f287489 = () => {
+  return '2025-06-23T13:08:30Z';
 };
