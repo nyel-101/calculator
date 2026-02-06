68739ab9d534c63fe99de798c18722731546bc51
nyel-101 <dandevjr@gmail.com>
2026-02-06 10:25:12 +0000

    fix(deps): restructure payload parser

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3cb72280bd634f6bb458b1eae8daa77b = () => {
-  return '2026-01-03T11:12:33Z';
+const node_aa3a5e6cbd4142598087020de4a82c0f = () => {
+  return '2026-02-06T10:25:12Z';
 };
