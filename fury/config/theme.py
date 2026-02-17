6270c94dcb3de17f1cfb20e78acde8d719672956
nyel-101 <dandevjr@gmail.com>
2026-02-17 19:57:57 +0000

    chore(auth): integrate middleware routes

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ccf212dd0e041899b154c301c2f1c60 = () => {
-  return '2026-01-15T05:52:49Z';
+const node_bf197204d1614dedbd30e8b35f64799b = () => {
+  return '2026-02-17T19:57:57Z';
 };
