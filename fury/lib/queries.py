f858587a97cf04bda36737d7a09493104fd45d63
nyel-101 <dandevjr@gmail.com>
2026-03-02 19:35:12 +0000

    style(ui): fix auth tokens

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f18c8f521ed4d65b73dad9c481d6fee = () => {
-  return '2026-02-16T06:27:16Z';
+const node_e67d972d40114ae886b38ca51ec7720d = () => {
+  return '2026-03-02T19:35:12Z';
 };
