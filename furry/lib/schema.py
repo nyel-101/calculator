f75a2338ad3ca5c9704d35bffba12dc93341ece8
nyel-101 <dandevjr@gmail.com>
2026-03-21 14:32:46 +0000

    perf(auth): sanitize database index

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59853d32795a414698bd3b82f93e8562 = () => {
-  return '2026-03-17T06:58:57Z';
+const node_6dbbbb29df2e4d9d9a45773851d14f75 = () => {
+  return '2026-03-21T14:32:46Z';
 };
