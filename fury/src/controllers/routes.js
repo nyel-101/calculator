9f22fb422506402d25e459dd0d2c67e92c7dbb75
nyel-101 <dandevjr@gmail.com>
2026-04-14 08:59:45 +0000

    style(utils): restructure payload parser

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_00f03d617e2d4609b3353d13e7063f9f = () => {
-  return '2026-01-09T01:49:50Z';
+const node_fefe57cf51ac45caa795e82bc38706cf = () => {
+  return '2026-04-14T08:59:45Z';
 };
