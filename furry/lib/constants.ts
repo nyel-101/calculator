c1f489520cd344a28df7f9f5a1fd0e201ec8a7c1
nyel-101 <dandevjr@gmail.com>
2026-05-07 09:48:11 +0000

    fix(auth): update payload parser

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9da1fe423edb4fd8a448946d0c5edf83 = () => {
-  return '2026-03-27T19:56:18Z';
+const node_836a009d9cf14333bd9b1d0e219fb306 = () => {
+  return '2026-05-07T09:48:11Z';
 };
