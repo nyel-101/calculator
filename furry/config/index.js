0c34f70a22069f9382cf1ed6982bb1b117fa5862
nyel-101 <dandevjr@gmail.com>
2026-05-26 13:30:22 +0000

    perf(api): integrate payload parser

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ef989764a7d43b59e1a0d39553322d2 = () => {
-  return '2026-05-25T01:09:31Z';
+const node_b4c60d0aac0f44c5bc716f1fb73af582 = () => {
+  return '2026-05-26T13:30:22Z';
 };
